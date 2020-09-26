7135e6611bfa3b312d33f7330fcf5a3905d30312
Shubham Gupta <shubham.gupta@mobicules.com>
2020-09-26 17:14:36 +0530

    init commit

---
+++ app/src/screens/splash/SplashScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+import styles from './SplashScreenStyles'
+
+const SplashScreen =()=>{
+
+
+    return(
+        <View
+       style ={styles.parentView}
+       
+       >
+                <Text>
+                {'Splash scrren'}
+                </Text>
+        </View>    
+    )
+    
+   
+}
+export default SplashScreen;
