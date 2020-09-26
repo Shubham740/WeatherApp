0762f127e46b1bde6739b3acc77a3f3972c8a137
Shubham Gupta <shubham.gupta@mobicules.com>
2020-09-26 17:16:43 +0530

    add main screen

---
+++ app/src/screens/mainScreen/MainScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+import styles from './SplashScreenStyles'
+
+const MainScreen =()=>{
+
+
+    return(
+        <View
+       style ={styles.parentView}
+       
+       >
+                <Text>
+                {'Main scrren'}
+                </Text>
+        </View>    
+    )
+    
+   
+}
+export default MainScreen;
